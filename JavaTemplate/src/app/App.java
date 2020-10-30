590c46425e178ea24c161bbae59c69f56bf6e23c
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-10-30 17:28:26 +0000

    Created Assignment

--- JavaTemplate/src/app/App.java
+++ JavaTemplate/src/app/App.java
@@ -1,7 +1,30 @@
+/*
+    ARRAYS CHALLENGE
+
+    Create a program that gives the user information penguin conservation status.
+    There are currently 18 different species of penguin inhabiting the earth.
+    Each species is in one of four catergories of conservation: Least Concern, Vulnerable, Near Threatened, and Endangered.
+    Ask the user to enter a species of penguin, and print out its conservation status.
+    A chart containing the penguin information you will need to ocmplete this assignment is in the explorer to the left.
+    
+    LIMITATIONS:
+    No more than 4 for loops
+    No more than 4 if statements
+    0 while loops
+    No more than 4 arrays
+
+    EXAMPLE:
+
+    >Please enter a species of penguin
+    >Gentoo
+    >The Gentoo Penguin is Least Concern
+
+*/
+
 package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Hello Penguin");
     }
 }
